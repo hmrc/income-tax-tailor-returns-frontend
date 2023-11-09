5410c42e8c00de106bd920e564f8e44295d587a8
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2023-11-09 14:19:27 +0000

    SASS-6092 Scaffold Income from work and taxable state benefits

---
+++ app/forms/workandbenefits/JobseekersAllowanceFormProvider.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.workandbenefits
+
+import forms.mappings.Mappings
+import models.workandbenefits.JobseekersAllowance
+import play.api.data.Form
+import play.api.data.Forms.set
+
+import javax.inject.Inject
+
+class JobseekersAllowanceFormProvider @Inject() extends Mappings {
+
+  def apply(isAgent: Boolean): Form[Set[JobseekersAllowance]] = {
+    val error: String =
+      if (isAgent) {
+        "jobseekersAllowance.agent.error.required"
+      } else {
+        "jobseekersAllowance.error.required"
+      }
+    Form(
+      "value" -> set(enumerable[JobseekersAllowance](error)).verifying(
+        firstError(nonEmptySet(error), exclusiveItemInSet(error, JobseekersAllowance.ExclusiveOption.toString))
+      )
+    )
+  }
+}
