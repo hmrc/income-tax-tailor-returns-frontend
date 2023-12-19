73b9b1bd7de62a5ed20af40bfe98ce174cf3e7df
ghousedev <49005870+ghousedev@users.noreply.github.com>
2023-12-19 09:41:44 +0000

    Formatting and removing commented out lines

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,12 +9,12 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"     % hmrcPlayFrontend,
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"             % hmrcPlayFrontend,
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30"  % "2.0.0",
-    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
-    "uk.gov.hmrc"                   %% "tax-year"                       % "3.3.0",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"           % "2.14.2"
+    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"             % bootstrapVersion,
+    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
+    "uk.gov.hmrc"                   %% "tax-year"                               % "3.3.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.14.2"
   )
 
   val test: Seq[ModuleID] = Seq(
