47b63c31a5e2447eaed97b3daaf750d3ade981bf
basotia <16934889+basotia@users.noreply.github.com>
2024-01-16 11:38:27 +0000

    radiopage template fixed with common code and tested

--- .g8/radioButtonPage/app/models/$className$.scala
+++ .g8/radioButtonPage/app/models/$className$.scala
@@ -8,39 +8,27 @@ sealed trait $className$
 
 object $className$ extends Enumerable.Implicits {
 
-  case object $option1key;
-  format = "Camel" $
-  extends WithName("$option1key;format="
-  decap"$"
-  ) with $className$
-
-  case object $option2key;
-  format = "Camel" $
-  extends WithName("$option2key;format="
-  decap"$"
-  ) with $className$
+  case object $option1key;format="Camel"$ extends WithName("$option1key;format="decap"$") with $className$
+  case object $option2key;format="Camel"$ extends WithName("$option2key;format="decap"$") with $className$
 
   val values: Seq[$className$] = Seq(
-    $option1key;
-  format = "Camel" $
-  , $option2key;
-  format = "Camel" $
+    $option1key;format="Camel"$, $option2key;format="Camel"$
   )
 
   private def getRadioItems(contentPrefix: String)(implicit messages: Messages): Seq[RadioItem] = {
     values.zipWithIndex.map {
       case (value, index) =>
         RadioItem(
-          content = Text(messages(s"$contentPrefix;format="decap"$.\${value.toString}") ),
+          content = Text(messages(contentPrefix+s".\${value.toString}") ),
           value = Some(value.toString),
           id = Some(s"value_\$index")
       )
     }
   }
 
-  def options(implicit messages: Messages): Seq[RadioItem] = getRadioItems(s"$className")
+  def options(implicit messages: Messages): Seq[RadioItem] = getRadioItems("$className;format=" decap"$")
 
-  def agentOptions(implicit messages: Messages): Seq[RadioItem] = getRadioItems(s"$className.agent")
+  def agentOptions(implicit messages: Messages): Seq[RadioItem] = getRadioItems("$className;format=" decap"$.agent")
 
   implicit val enumerable: Enumerable[$className$] =
     Enumerable(values.map(v => v.toString -> v): _*)
