abbe3c3e6094b19daff60f1e26f9ffb92a48b56a
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2025-02-07 14:49:34 +0000

    SASS-11074: Add PrePopulationService testing.

--- test/services/PrePopulationServiceSpec.scala
+++ test/services/PrePopulationServiceSpec.scala
@@ -17,8 +17,55 @@
 package services
 
 import base.SpecBase
+import mocks.MockStateBenefitsConnector
+import models.errors.SimpleErrorWrapper
+import models.prePopulation.StateBenefitsPrePopulationResponse
+import play.api.http.Status.{IM_A_TEAPOT, INTERNAL_SERVER_ERROR}
+import play.api.test.DefaultAwaitTimeout
+import play.api.test.Helpers.await
+import uk.gov.hmrc.http.HeaderCarrier
 
-class PrePopulationServiceSpec extends SpecBase {
+class PrePopulationServiceSpec extends SpecBase
+  with MockStateBenefitsConnector
+  with DefaultAwaitTimeout {
 
+  val nino: String = "AA111111A"
+  override val taxYear: Int = 2025
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  val testService = new PrePopulationService(
+    stateBenefitsConnector = mockStateBenefitsConnector
+  )
+
+  val dummyResponse: StateBenefitsPrePopulationResponse = StateBenefitsPrePopulationResponse(
+    hasEsaPrePop = true,
+    hasJsaPrePop = true,
+    hasPensionsPrePop = true,
+    hasPensionLumpSumsPrePop = true
+  )
+
+  "getStateBenefits" -> {
+    "should return a success response when connector returns success response" in {
+      mockGetPrePopulation(nino, taxYear, Right(dummyResponse))
+      val result = await(testService.getStateBenefits(nino, taxYear))
+      result mustBe a[Right[_, _]]
+      result.getOrElse(StateBenefitsPrePopulationResponse.empty) mustBe dummyResponse
+    }
+
+    "should return an error response when connector returns error response" in {
+      mockGetPrePopulation(nino, taxYear, Left(SimpleErrorWrapper(INTERNAL_SERVER_ERROR)))
+      val result = await(testService.getStateBenefits(nino, taxYear))
+      result mustBe a[Left[_, _]]
+      result.swap.getOrElse(SimpleErrorWrapper(IM_A_TEAPOT)).status mustBe INTERNAL_SERVER_ERROR
+    }
+
+    "should throw an exception when an exception is thrown by the connector" in {
+      mockGetPrePopulationException(nino, taxYear, new RuntimeException())
+      assertThrows[RuntimeException](
+        await(testService.getStateBenefits(nino, taxYear))
+      )
+    }
+  }
 }
 
