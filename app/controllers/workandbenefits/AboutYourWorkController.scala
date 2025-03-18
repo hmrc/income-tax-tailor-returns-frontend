7726a59e02a96852184865f0cf2e5f0ac8af6037
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2025-03-18 13:50:46 +0000

    SASS-11074: Non-foster carer journey pre-pop is not in scope. Removing pre-pop functionality from that controller

--- app/controllers/workandbenefits/AboutYourWorkController.scala
+++ app/controllers/workandbenefits/AboutYourWorkController.scala
@@ -16,96 +16,86 @@
 
 package controllers.workandbenefits
 
-import config.FrontendAppConfig
-import controllers.ControllerWithPrePop
 import controllers.actions.TaxYearAction.taxYearAction
 import controllers.actions._
 import forms.workandbenefits.AboutYourWorkFormProvider
-import handlers.ErrorHandler
 import models.Mode
-import models.prePopulation.EmploymentPrePopulationResponse.EmploymentPrePop
 import models.requests.DataRequest
 import models.workandbenefits.AboutYourWork
 import navigation.Navigator
 import pages.workandbenefits.AboutYourWorkPage
 import play.api.data.Form
-import play.api.i18n.MessagesApi
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.libs.json.Format.GenericFormat
 import play.api.mvc._
-import play.twirl.api.HtmlFormat
-import services.{PrePopulationService, SessionDataService, UserDataService}
-import uk.gov.hmrc.http.HeaderCarrier
-import utils.Logging
+import services.UserDataService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.workandbenefits.{AboutYourWorkAgentView, AboutYourWorkView}
 
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.ExecutionContext
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
-@Singleton
 class AboutYourWorkController @Inject()(override val messagesApi: MessagesApi,
-                                        val userDataService: UserDataService,
-                                        prePopService: PrePopulationService,
-                                        val ninoRetrievalService: SessionDataService,
-                                        val config: FrontendAppConfig,
-                                        val navigator: Navigator,
-                                        val identify: IdentifierActionProvider,
-                                        val getData: DataRetrievalActionProvider,
-                                        val requireData: DataRequiredActionProvider,
-                                        val overrideRequestActionProvider: OverrideRequestActionProvider,
-                                        val formProvider: AboutYourWorkFormProvider,
+                                        userDataService: UserDataService,
+                                        navigator: Navigator,
+                                        identify: IdentifierActionProvider,
+                                        getData: DataRetrievalActionProvider,
+                                        requireData: DataRequiredActionProvider,
+                                        overrideRequestActionProvider: OverrideRequestActionProvider,
+                                        formProvider: AboutYourWorkFormProvider,
                                         val controllerComponents: MessagesControllerComponents,
                                         view: AboutYourWorkView,
-                                        agentView: AboutYourWorkAgentView,
-                                        val errorHandler: ErrorHandler
-                                       )(implicit val ec: ExecutionContext)
-  extends ControllerWithPrePop[Set[AboutYourWork], EmploymentPrePop]
-    with Logging {
+                                        agentView: AboutYourWorkAgentView)
+                                       (implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  override protected val primaryContext: String = "AboutYourWorkController"
-  override val defaultPrePopulationResponse: EmploymentPrePop = EmploymentPrePop.empty
-
-  override protected def actionChain(taxYear: Int,
-                                     requestOverrideOpt: Option[DataRequest[_]] = None): ActionBuilder[DataRequest, AnyContent] =
-    requestOverrideOpt.map(overrideRequestActionProvider(_)).getOrElse(
+  private def actionChain(taxYear: Int, optionalRequestOverride: Option[DataRequest[_]]): ActionBuilder[DataRequest, AnyContent] =
+    optionalRequestOverride.map(overrideRequestActionProvider(_)).getOrElse(
       identify(taxYear) andThen taxYearAction(taxYear) andThen getData(taxYear) andThen requireData(taxYear)
     )
 
-  override protected def prePopRetrievalAction(nino: String, taxYear: Int, mtdItId: String)
-                                              (implicit hc: HeaderCarrier): PrePopResult =
-    () => prePopService.getEmployment(nino, taxYear, mtdItId).map(_.map(_.toPrePopModel))
-
-  override protected def viewProvider(form: Form[_],
-                                      mode: Mode,
-                                      taxYear: Int,
-                                      prePopData: EmploymentPrePop)
-                                     (implicit request: DataRequest[_]): HtmlFormat.Appendable =
-    if (request.isAgent) {
-      agentView(form, mode, taxYear)
-    } else {
-      view(form, mode, taxYear)
-    }
-
-  val pageName = "Employment" //TODO: Change this
-  val incomeType = "employment" //TODO: Change this
+  def form(isAgent: Boolean): Form[Set[AboutYourWork]] = formProvider(isAgent)
 
   def onPageLoad(mode: Mode,
                  taxYear: Int,
-                 requestOverrideOpt: Option[DataRequest[_]]): Action[AnyContent] = onPageLoad(
-    pageName = pageName,
-    incomeType = incomeType,
-    page = AboutYourWorkPage,
-    mode = mode,
-    taxYear = taxYear,
-    requestOverrideOpt = requestOverrideOpt
-  )
+                 optionalRequestOverride: Option[DataRequest[_]]): Action[AnyContent] =
+    actionChain(taxYear, optionalRequestOverride) {
+      implicit request =>
+
+        def deriveView[A](form: Form[A], status: Status): Result = getView(mode, taxYear)(form, status)
+
+        val preparedCheckboxForm = request.userAnswers.get(AboutYourWorkPage) match {
+          case None => form(request.isAgent)
+          case Some(value) => form(request.isAgent).fill(value)
+        }
+
+        deriveView(preparedCheckboxForm, Ok)
+    }
 
   def onSubmit(mode: Mode,
                taxYear: Int,
-               requestOverrideOpt: Option[DataRequest[_]]): Action[AnyContent] = onSubmit(
-    pageName = pageName,
-    incomeType = incomeType,
-    page = AboutYourWorkPage,
-    mode = mode,
-    taxYear = taxYear,
-    requestOverrideOpt = requestOverrideOpt
-  )
+               optionalRequestOverride: Option[DataRequest[_]]): Action[AnyContent] =
+    actionChain(taxYear, optionalRequestOverride).async {
+      implicit request =>
+
+        def deriveView[A](form: Form[A], status: Status): Result = getView(mode, taxYear)(form, status)
+
+        form(request.isAgent).bindFromRequest().fold(
+          formWithErrors =>
+            Future.successful(deriveView(formWithErrors, BadRequest)),
+
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(AboutYourWorkPage, value))
+              _ <- userDataService.set(updatedAnswers, request.userAnswers)
+            } yield Redirect(navigator.nextPage(AboutYourWorkPage, mode, updatedAnswers))
+        )
+    }
+
+  private def getView[A](mode: Mode, taxYear: Int)(form: Form[A], status: Status)
+                        (implicit request: DataRequest[_], messages: Messages): Result =
+    if (request.isAgent) {
+      status(agentView(form, mode, taxYear)(request, messages))
+    } else {
+      status(view(form, mode, taxYear)(request, messages))
+    }
 }
